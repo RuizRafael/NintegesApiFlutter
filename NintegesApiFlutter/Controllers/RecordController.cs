7b90164047b7e1c799569f83bf57ca61e5c2d78b
Rafael Ruiz <64376937+RuizRafael@users.noreply.github.com>
2023-06-29 19:44:41 +0200

    record again

--- NintegesApiFlutter/Controllers/RecordController.cs
+++ NintegesApiFlutter/Controllers/RecordController.cs
@@ -51,7 +51,7 @@ namespace NintegesApiFlutter.Controllers
         public JsonResult InsertRecord(string type,int userId,int workplaceId,int isSameLocation, string note)
         {
             //string query = @"insert into record (type,userId,workplaceId,isSameLocation,note) values (@type,@userId,@workplaceId,@isSameLocation,@note";
-            string query = $"insert into record (type,userId,workplaceId,isSameLocation,note) values ('{record.type}','{record.userId}','{record.workplaceId}','{record.isSameLocation}','{record.note}')";
+            string query = $"insert into record (type,userId,workplaceId,isSameLocation,note) values ('{type}','{userId}','{workplaceId}','{isSameLocation}','{note}')";
 
             DataTable table = new DataTable();
             string sqlDataSource = configuration.GetConnectionString("sqlCon");
@@ -76,32 +76,7 @@ namespace NintegesApiFlutter.Controllers
             return new JsonResult("Added successfully");
         }
 
-        [HttpPost]
-        [Route("[action]")]
-        public JsonResult InsertRecordParams(int type,int userId,int workplaceId,int isSameLocation, string note)
-        {
-            //string query = @"insert into record (type,userId,workplaceId,isSameLocation,note) values (@type,@userId,@workplaceId,@isSameLocation,@note";
-            string query = $"insert into record (type,userId,workplaceId,isSameLocation,note) values ('{type}','{userId}','{workplaceId}','{isSameLocation}','{note}')";
-
-            DataTable table = new DataTable();
-            string sqlDataSource = configuration.GetConnectionString("sqlCon");
-            MySqlDataReader reader;
-            using (MySqlConnection conection = new MySqlConnection(sqlDataSource))
-            {
-                conection.Open();
-                using (MySqlCommand command = new MySqlCommand(query, conection))
-                {
-                    reader = command.ExecuteReader();
-                    table.Load(reader);
-
-                    reader.Close();
-                    conection.Close();
-                }
-            }
-            return new JsonResult("Added successfully");
-        }
-
-
+    
 
     }
 }
